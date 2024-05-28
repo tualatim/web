87b12391d4501a58fdfdc622aacd125612a045e7
Vinícius Gomes Marques <viniciusgm.marques@gmail.com>
2024-05-27 23:03:04 -0300

    atualizando web-pratica2

--- web-pratica2/src/App.jsx
+++ web-pratica2/src/App.jsx
@@ -1,14 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useContext, userState } from 'react';
+import Notes from './pages/notes';
+import SignIn from './pages/signin';
+import Login from './pages/login';
 
 import './App.css'
 
-function App() {
+export default function App() {
 
+    const { user } = ();
+  
+    return (
+      <>
+        <BrowserRouter>
+          <Routes>
+            {user.logado ? (
+              <Route element={<Layout />}>
+                <Route path="/" element={<Notes />} />
+              </Route>
+            ) : (
+              <Route path="/signin" element={<SignIn />} />
+            )}
+              <Route path="*" element={<p>Página não Encontrada</p>} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    );
+  }
 
-  return (
-    <>
-      
-    </>
-  )
-}
-
-export default App
