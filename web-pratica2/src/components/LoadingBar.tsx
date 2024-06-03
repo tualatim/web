c90ca6c66fe53e2a6764a3aaf1cbfc06ad5c17f0
Victor Laurentino do Nascimento <53540002+PitzTech@users.noreply.github.com>
2024-06-03 19:02:32 -0300

    add loading bar

---
+++ web-pratica2/src/components/LoadingBar.tsx
@@ -0,0 +1,5 @@
+export const LoadingBar = () => (
+  <div className="progress">
+    <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow={100} aria-valuemin={0} aria-valuemax={100} style={{ width: "100%" }}></div>
+  </div>
+)
